5dd30e9d765472e53de2479a13196801ed2ab14c
likithavijjapu <32402497+likithavijjapu@users.noreply.github.com>
2018-01-11 11:16:12 -0800

    Create BlockThread.cu

---
+++ vectorTest/BlockThread.cu
@@ -0,0 +1,62 @@
+# include <time.h>
+# include <math.h>
+# include <stdio.h>
+
+__global__ void add( int *a , int *b , int *c)
+{
+	int index= threadIdx.x + blockIdx.x * blockDim.x;
+	c[index] = a[index] +b[index];
+	
+}
+
+//# define N 125
+#define thread_count 10
+
+void random_ints(int* a, int h)
+{
+
+}
+
+int main(void){
+	int N;
+	printf("enter size of vector\n");
+	scanf("%d",&N);
+	int a[N],b[N],c[N];
+	int *d_a,*d_b,*d_c;
+
+	int size = N * sizeof(int);
+	
+	cudaMalloc((void **)&d_a, size);
+	cudaMalloc((void **)&d_b, size);
+	cudaMalloc((void **)&d_c, size);
+	for ( int i=0;i<=N;i++)
+  		{
+    		a[i]=i+2;
+    		b[i]=i+3;
+   		c[i]=0;
+  		}
+
+	
+	
+	
+	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_c, c, size, cudaMemcpyHostToDevice);
+	clock_t start_time = clock(); 
+	add<<<N,thread_count>>>(d_a, d_b, d_c);
+	cudaThreadSynchronize(); 
+	clock_t stop_time = clock();
+	int time =stop_time - start_time;
+	printf("time=%d\n", time);
+
+	cudaMemcpy(c,d_c,size,cudaMemcpyDeviceToHost);
+	printf("c=");
+	for(int i=0;i<N;i++){
+	
+	printf("%d+",c[i]);}
+	printf("\n");
+
+	
+	cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+	return 0;
+}

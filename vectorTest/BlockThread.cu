72d315f2a25c3278185b658c631158b8ace6d8f3
likithavijjapu <32402497+likithavijjapu@users.noreply.github.com>
2018-01-11 11:43:01 -0800

    Update BlockThread.cu

--- vectorTest/BlockThread.cu
+++ vectorTest/BlockThread.cu
@@ -21,14 +21,15 @@ int main(void){
 	int N;
 	printf("\"Hello Vector !\"\n enter size of vector\n");
 	scanf("%d",&N);
-	int a[N],b[N],c[N];
-	int *d_a,*d_b,*d_c;
+	int a[N],b[N],c[N];  // host copies of a, b,c
+	int *d_a,*d_b,*d_c;  //// device copies of a, b, c
 
 	int size = N * sizeof(int);
-	
+	// Alloc space for device copies of a, b, c
 	cudaMalloc((void **)&d_a, size);
 	cudaMalloc((void **)&d_b, size);
 	cudaMalloc((void **)&d_c, size);
+	//setup input values for a, b, c  
 	for ( int i=0;i<=N;i++)
   		{
     		a[i]=i+2;
@@ -37,18 +38,20 @@ int main(void){
   		}
 
 	
-	
-	
+	// Copy inputs to device
 	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_c, c, size, cudaMemcpyHostToDevice);
+	// start clocking
 	clock_t start_time = clock(); 
+	//Launch add() kernel on GPU
 	add<<<N,thread_count>>>(d_a, d_b, d_c);
 	cudaThreadSynchronize(); 
+	//end clocking and measuring time for execution
 	clock_t stop_time = clock();
 	int time =stop_time - start_time;
 	printf("time=%d\n", time);
-
+	//Copy result back to host
 	cudaMemcpy(c,d_c,size,cudaMemcpyDeviceToHost);
 	printf("c=");
 	for(int i=0;i<N;i++){
@@ -56,7 +59,7 @@ int main(void){
 	printf("%d+",c[i]);}
 	printf("\n");
 
-	
+	// Cleanup
 	cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
 	return 0;
 }
